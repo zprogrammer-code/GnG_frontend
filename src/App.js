7b20eb42b76744e207be7c5cf213f715df94fd61
Zprogrammer-code <skatboyza124@hotmail.com>
2021-04-16 02:39:28 -0600

    just added a form/home page for signing u- or logging in. I added some css but just installed some dependancies. I am now trying to import the home component to my app.js page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+import Home from '/home';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+    <Switch>
+      < Route
+      exact
+      path={"/"}
+      render={props => (
+        <Home /> 
+        )}
+      />
+   
+      < Route
+       exact 
+       path={"/dashboard"}
+       render={props => (
+        < Dashboard /> 
+        )}
+      />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
